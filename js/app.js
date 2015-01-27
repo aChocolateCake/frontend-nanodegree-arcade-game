7efcfdd26a67488c0de714fa30a64ac5faa22cee
Katie <katie@digikatie.com>
2015-01-26 20:46:58 -0600

    rudimentery gameplay achieved. no fancy bits.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,100 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// static y positions to use 
+var posY = [60,142,225];
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// gets random from posY
+var randomY = function(){
+    return posY[Math.floor(Math.random()*posY.length)];
+}
+
+// enemy class. enemies our player must avoid
+var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
+    this.x = -50;
+    this.y = randomY();//posY[Math.floor(Math.random()*posY.length)];
+    this.speed = Math.floor(Math.random() * 300) + 100;
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
+    // collision detection
+    if((this.x + 100 >= player.x && this.x + 100 <= player.x + 75) && (this.y + 70 >= player.y && this.y <= player.y + 60)){
+        player.resetPos();
+    }
+    // moves enemy if it isn't at the end of playable area 
+    if (this.x < 550){
+        this.x += (this.speed * dt);
+    // moves enemy to beginning again if at end of playable area    
+    } else if (this.x > 550){
+        this.x = -50;
+        this.y = randomY();//posY[Math.floor(Math.random()*posY.length)];
+        }
+    }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// player class. This class requires an update(), render() and a handleInput() method
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = 200;
+    this.y = 400;
+}
 
+// spawns enemies
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// spawns player
+var player = new Player();
 
+// Update player movement
+Player.prototype.update = function(dt) {
+    this.x * dt + this.speed;
+    this.y * dt + this.speed;
 
+    // checks if player has hit water
+    if(this.y <= 10){
+        this.resetPos();
+    }
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// checks for win scenerio (player hits water)
+Player.prototype.win = function(){
+    if(this.y <= 10){
+        this.resetPos();
+    }
+}
+
+// renders the player image
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+}
+// resets player position
+Player.prototype.resetPos = function() {
+  this.x = 200;
+  this.y = 400;
+}
+
+// player movement
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress === 'up' && this.y > 10) {
+        this.y -= 85;
+    }
+    if (keyPress === 'down' && this.y < 400) {
+        this.y += 85;
+    }
+    if (keyPress === 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if (keyPress === 'left' && this.x > 0) {
+        this.x -= 100;
+    }
+}
+
+// listens for key presses and sends the keys to Player.handleInput()
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
